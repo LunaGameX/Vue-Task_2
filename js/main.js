0fa1e5ca45746dbd9f8f37f839a61c1067a9a324
LunaGameX <>
2023-11-16 10:24:28 +0300

    "initial project vue_task2"

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let eventBus = new Vue()
+
+Vue.component('columns', {
+    template: `
+    
+    `,
+    data() {
+
+    },
+    mounted() {
+
+    },
+
+
+})
+
+
+
+let app = new Vue({
+    el: '#app',
+    data: {}
+})
